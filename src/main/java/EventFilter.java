77b9859b6205a1fde0b8f850580dc6bf2c772ec7
DavidIonescu17 <davidionescu365@gmail.com>
2025-02-18 10:57:44 +0200

    b done

--- src/main/java/EventFilter.java
+++ src/main/java/EventFilter.java
@@ -1,2 +1,13 @@
+import java.util.List;
+
 public class EventFilter {
+    public static void filterEvent(java.util.List<Event> events, double points){
+        events.stream()
+                .filter(event -> event.getPunkte()>points)
+                .map(Event::getName)
+                .distinct()
+                .forEach(System.out::println);
+
+
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -6,6 +7,8 @@ public class Main {
     public static void main(String[] args) {
         String fileName = "src/main/java/ninja_events.tsv";
         List<Event> events = EventReader.readData(fileName);
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give point input to filter by: ");
+        EventFilter.filterEvent(events,Double.parseDouble(scanner.nextLine()));
     }
 }
